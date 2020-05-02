0bb2c79b29795edf958f4230bb3ef27d806f866c
SMoore3773 <Smooresculpture@gmail.com>
2020-05-02 12:57:47 -0400

    login creds right format, 404 on login submit

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -6,8 +6,19 @@ import ColorList from "./ColorList";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+
+  useEffect(()=>{
+    axios
+    .get('http://localhost:5000/api/colors')
+    .then(res=>{
+      console.log('resin axios get on bubblePage ',res);
+      setColorList(res.data)
+    })
+    .catch(err=>console.log('error in axios get ',err))
+  },[])
 
   return (
     <>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
+import {axiosWithAuth} from '../utils/axiosWithAuth';
+import {useHistory} from 'react-router-dom';
 
-const Login = () => {
+
+const Login = (props) => {
+  const [loginCreds, setLoginCreds] = useState({credentials:{username:'', password:''}})
+  const history = useHistory();
+console.log(history)
+
+  const handleChanges = e =>{
+    setLoginCreds({
+      credentials:{...loginCreds.credentials,
+        [e.target.name]:e.target.value
+      }
+    })
+  };
+console.log(loginCreds)
+  const login = e =>{
+    e.preventDefault();
+    axiosWithAuth()
+    .post('http://localhost:5000/api.login',loginCreds)
+    .then(res=>{
+      console.log('login success in login.js axios post', res)
+      localStorage.setItem('token', res.data.payload);
+      history.push('/bubble-page')
+    })
+    .catch(err => console.log('error in login : ',err))
+  }
+
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
+      <form className='loginForm' onSubmit={login}>
+                    <input
+                    type='text'
+                    name='username'
+                    value={loginCreds.credentials.username}
+                    onChange={handleChanges}
+                    placeholder='Username'
+                    className='loginField'
+                    />
+                    <input
+                    type='text'
+                    name='password'
+                    value={loginCreds.credentials.password}
+                    onChange={handleChanges}
+                    placeholder='Password'
+                    className='loginField'
+                    />
+                <button className='loginButton'> Login </button>
+                </form>
     </>
   );
 };
